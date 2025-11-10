0a6c2e75675709bd321867d639d16ffa44f95174
Anthony Cox <ethanrcox@gmail.com>
2025-11-10 16:45:39 -0500

    Updated HealthComponent to have is_alive

--- src/scene/combat_system/health_component.gd
+++ src/scene/combat_system/health_component.gd
@@ -7,22 +7,31 @@ signal on_heal(initial_health: float, amount: float)
 signal on_damage(initial_health: float, amount: float)
 signal on_death
 
+var _is_alive: bool
+
+func _init(is_alive_: bool = true) -> void:
+	_is_alive = is_alive_
+
+func is_alive() -> bool:
+	return _is_alive
+
 func get_health() -> float:
 	return _health.value
 	
 func heal(amount: float) -> float:
-	if amount > 0.0:
+	if _is_alive && amount > 0.0:
 		on_heal.emit(_health.value, amount)
 		_health.value += amount
 
 	return _health.value
 
 func damage(amount: float) -> float:
-	if amount > 0.0:
+	if _is_alive && amount > 0.0:
 		on_damage.emit(_health.value, amount)
 		_health.value -= amount
 		
 	if _health.value <= 0.0:
+		_is_alive = false
 		on_death.emit()
 	
 	return _health.value
--- test/scene/combat_system/health_component_test.gd
+++ test/scene/combat_system/health_component_test.gd
@@ -11,13 +11,13 @@ const __source: String = 'res://src/scene/combat_system/health_component.gd'
 var health_component: HealthComponent
 
 func before():
-	health_component = monitor_signals(HealthComponent.new())
+	health_component = monitor_signals(HealthComponent.new(true))
 	health_component._health = auto_free(ResourceComponent.new())
-	
+
 ## HealthComponent::heal() should:
 ##	- Incease health by ceiling(maximum health, current health + healing) if the amount of healing is positive
 ##  - Do nothing if the amount of healing is non-positive
-func test_healing_with_amounts(initial_health: float, healing: float, expected_health: float, test_parameters := [
+func test_heal_with_amounts(initial_health: float, healing: float, expected_health: float, test_parameters := [
 	[50.0, -1.0, 50.0],
 	[50.0, 0.0, 50.0],
 	[50.0, 1.0, 51.0]
@@ -26,6 +26,15 @@ func test_healing_with_amounts(initial_health: float, healing: float, expected_h
 	var actual_health: float = health_component.heal(healing)
 	assert_float(actual_health).is_equal(expected_health)
 
+## HealthComponent::heal() should do nothing if the entity is not alive
+func test_heal_with_alive_values(initial_health: float, is_alive: bool, healing: float, expected_health: float, test_parameters := [
+	[50.0, true, 10.0, 60.0],
+	[50.0, false, 10.0, 50.0]
+]) -> void:
+	health_component._health.value = initial_health
+	health_component._is_alive = is_alive
+	var actual_health: float = health_component.heal(healing)
+	assert_float(actual_health).is_equal(expected_health)
 
 ## HealthComponent::damage() should:
 ##	- Decrease health by floor(mininum health, current health - damage) if the amount of damage is positive
@@ -38,3 +47,26 @@ func test_damage_with_amounts(initial_health: float, damage: float, expected_hea
 	health_component._health.value = initial_health
 	var actual_health: float = health_component.damage(damage)
 	assert_float(actual_health).is_equal(expected_health)
+
+## HealthComponent::damage() should do nothing if the entity is not alive
+func test_damage_with_alive_values(initial_health: float, is_alive: bool, damage: float, expected_health: float, test_parameters := [
+	[50.0, true, 10.0, 40.0],
+	[50.0, false, 10.0, 50.0]
+]) -> void:
+	health_component._health.value = initial_health
+	health_component._is_alive = is_alive
+	var actual_health: float = health_component.damage(damage)
+	assert_float(actual_health).is_equal(expected_health)
+
+## HealthComponent::damage() should update _is_alive iff health equals 0.0
+func test_damage_sets_is_alive_on_death(initial_health: float, damage: float, expected_health: float, expected_is_alive: bool, test_parameters := [
+	[50.0, 10.0, 40.0, true],
+	[50.0, 50.0, 0.0, false],
+	[50.0, 49.99, 0.01, true]
+]) -> void:
+	health_component._health.value = initial_health
+	health_component._is_alive = true
+	
+	var actual_health: float = health_component.damage(damage)
+	assert_float(actual_health).is_equal_approx(expected_health, 0.01)
+	assert_bool(health_component._is_alive).is_equal(expected_is_alive)
