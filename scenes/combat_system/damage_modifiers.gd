77361dfe3be08520bcab9651ee042d2264ec270d
Michael Gain <gainmikey@gmail.com>
2025-11-20 20:56:24 -0500

    stuff

---
+++ scenes/combat_system/damage_modifiers.gd
@@ -0,0 +1,4 @@
+class_name DamageModifiers
+extends Node
+
+@export var Modifiers : Dictionary[DamageType, float]
--- src/scene/player/player.gd
+++ src/scene/player/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
-@export var movement_speed: float = 500.0
+signal on_damage(amount: int, damage_type: DamageType, source: Node)
 
+
+@export var movement_speed: float = 500.0
 var _movement_direction : Vector2 = Vector2.ZERO
 
-func _process(delta: float):
+
+func _process(_delta: float):
 	look_at(get_global_mouse_position())
 	
 	_movement_direction.x = Input.get_axis("move_left", "move_right")
@@ -20,3 +23,7 @@ func _process(delta: float):
 		if %Sprite.animation != "Idle": %Sprite.animation = "Idle"
 
 	move_and_slide()
+	
+	
+func Damage(amount: int, damage_type: DamageType, source: Node):
+	on_damage.emit(amount, damage_type, source)
